1f9fcdeb0827f296baaa018b0fb70efba670bf69
Mitch Denny <mitch@mitchdenny.com>
2015-11-10 21:32:16 +1100

    Hooked up the encode and decode commands.

--- extension.ts
+++ extension.ts
@@ -1,24 +1,24 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode'; 
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "ecdc" is now active!'); 
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with  registerCommand
-	// The commandId parameter must match the command field in package.json
-	var disposable = vscode.commands.registerCommand('extension.sayHello', () => {
-		// The code you place here will be executed every time your command is executed
-
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World!');
+	var encodeSelectionDisposable = vscode.commands.registerTextEditorCommand('extension.encodeSelection', (textEditor, edit) => {
+		textEditor.edit((editBuilder) => {
+			editBuilder.replace(textEditor.selection, 'Encode');
+		}).then(() => {
+			vscode.window.showInformationMessage('Encoded!');
+		});
 	});
 	
-	context.subscriptions.push(disposable);
+	context.subscriptions.push(encodeSelectionDisposable);
+	
+	var decodeSelectionDisposable = vscode.commands.registerTextEditorCommand('extension.decodeSelection', (textEditor, edit) => {
+		textEditor.edit((editBuilder) => {
+			editBuilder.replace(textEditor.selection, 'Decode');
+		}).then(() => {
+			vscode.window.showInformationMessage('Decoded!');
+		});
+	});
+		
+	context.subscriptions.push(decodeSelectionDisposable);
 }
