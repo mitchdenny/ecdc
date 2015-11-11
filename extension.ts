c6faee1e78121da0a8758669e93addc333e52afe
Mitch Denny <mitch@mitchdenny.com>
2015-11-11 22:38:58 +1100

    Checking in some problem code to get a second opinion.

--- extension.ts
+++ extension.ts
@@ -1,6 +1,70 @@
 import * as vscode from 'vscode'; 
 
+function processSelections(textEditor: vscode.TextEditor, edit: vscode.TextEditorEdit, transform: (input: string) => string) {
+	
+	let updatedSelections: vscode.Selection[] = [];
+	let characterOffset = 0;
+	let lineOffset = 0;
+	
+	for (let selectionIndex = 0; selectionIndex < textEditor.selections.length; selectionIndex++) {
+		
+		let selection = textEditor.selections[selectionIndex];
+		let range = new vscode.Range(selection.start, selection.end);
+		let input = textEditor.document.getText(range);
+		let output = transform(input);
+		
+		characterOffset = output.length - input.length;
+		
+		edit.replace(selection, output);
+		
+		console.log('selection.start.line = %s', selection.start.line);
+		console.log('selection.start.character = %s', selection.start.character);
+		console.log('selection.end.line = %s', selection.end.line);
+		console.log('selection.end.character = %s', selection.end.character);
+		console.log('characterOffset = %s', characterOffset);
+		console.log('input.length = %s', input.length);
+		console.log('output.length = %s', output.length);
+		console.log('selection.start.character + output.length', selection.start.character + output.length);
+		
+		// BUG?? This code will select five characters.
+		let updatedSelection = new vscode.Selection(
+			selection.start.line,
+			selection.start.character,
+			selection.start.line,
+			8
+		);
+
+		// BUG?? This code will select three characters.
+		let updatedSelection = new vscode.Selection(
+			selection.start.line,
+			selection.start.character,
+			selection.start.line,
+			7
+		);
+		
+		// PROBLEM?? How do you select 4 characters.
+		// ADDITIONAL INFO: Only appears to happen when
+		// you are replacing a selection that overlaps
+		// the original selection.
+				
+		textEditor.selection = updatedSelection;	
+	}
+}
+
 export function activate(context: vscode.ExtensionContext) {
+
+	var selectionProblemDisposable = vscode.commands.registerTextEditorCommand('extension.selectionProblem', (textEditor, edit) => {
+		
+		processSelections(textEditor, edit, (input) => {
+			var buffer = new Buffer(input);
+			var output = buffer.toString('base64');
+			console.log('Converted %s to %s.', input, output);
+			return output;
+		});
+
+	});
+	
+	context.subscriptions.push(selectionProblemDisposable);
 
 	var encodeSelectionDisposable = vscode.commands.registerTextEditorCommand('extension.encodeSelection', (textEditor, edit) => {
 		textEditor.edit((editBuilder) => {
