f304b9853655d8f1f1a83bde0d69ecd04edf0a0c
Mitch Denny <mitch.denny@readify.net>
2015-12-11 16:06:07 +1100

    Working through the auto packaging process.

--- Gulpfile.js
+++ Gulpfile.js
@@ -2,7 +2,17 @@ var gulp = require('gulp');
 var fs = require('fs');
 
 gulp.task('default', function() {
-	var packageFileContents = fs.readFileSync('package.json');
-	console.log(packageFileContents);
-	console.log('This version is: ' + process.env.GITVERSION_FullSemVer);
+	var originalContents = fs.readFileSync('package.json', 'utf8');
+	var pkg = JSON.parse(originalContents);
+	
+	var version = process.env.GITVERSION_SemVer;
+	
+	if (version != null) {
+		pkg.version = process.env.GITVERSION_SemVer;		
+	} else {
+		pkg.version = '0.0.0';
+	}
+
+	var updatedContents = JSON.stringify(pkg, null, '\t');
+	console.log(updatedContents);
 })
