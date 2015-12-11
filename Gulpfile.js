3864f66d5f87fb78507f6ac0be929ed4eb46f601
Mitch Denny <mitch.denny@readify.net>
2015-12-11 14:27:22 +1100

    Reading the package file.

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,5 +1,8 @@
 var gulp = require('gulp');
+var fs = require('fs');
 
 gulp.task('default', function() {
+	var packageFileContents = fs.readFileSync('package.json');
+	console.log(packageFileContents);
 	console.log('This version is: ' + process.env.GITVERSION_FullSemVer);
 })
